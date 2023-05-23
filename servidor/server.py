984ae8df98faed6149458d61303d5fcf0bbf2250
JaredTrOr <trujillojaredalexander@gmail.com>
2023-05-23 09:15:51 -0600

    Prueba de implementación de API

--- servidor/server.py
+++ servidor/server.py
@@ -8,9 +8,14 @@ HOST = socket.gethostbyname(socket.gethostname())
 PORT = 5050
 ADDR = (HOST, PORT)
 
-conexiones = []
-direcciones = []
+#Nombre (identificadores) de los equipos traidos de la base de datos SQL
+equipos_existentes = [] 
 
+#Arreglo de conexiones realizadas en tiempo real
+conexiones_activas = []
+direcciones_activas = []
+
+#1er Hilo: Realizar el levantado del canal (socket) y aceptar nuevas conexiones
 def crear_socket():
     try:
         global servidor
@@ -28,17 +33,27 @@ def vincular_socket():
         print('Intentando denuevo...')
         vincular_socket()
 
-def aceptar_solicitudes():
+def aceptar_conexiones():
     while True:
         try:
             conn, addr = servidor.accept() #Linea que bloquea el flujo del programa
             servidor.setblocking(1) #Evita el tiempo de espera de las conexiones, (el servidor no se cierra)
 
             #Guardar las conexiones y direcciones del cliente
-            conexiones.append(conn)
-            direcciones.append(addr)
+            conexiones_activas.append(conn)
+            direcciones_activas.append(addr)
 
             print(f'Se ha establecido la conexión con {addr}')
 
         except:
             print('Error al aceptar la conexión :(')
+
+#2do Hilo: Encargado de administrar y manejar las funcionalidades de los usuarios ya existentes
+def panel_operaciones():
+    pass
+
+def listar_equipos():
+    pass
+
+def conectar_con_equipo():
+    pass
