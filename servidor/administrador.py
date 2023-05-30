032b7fd5085913c85e5e05120e71dcb5bde3a6d7
JaredTrOr <trujillojaredalexander@gmail.com>
2023-05-30 11:03:57 -0600

    Se cambio el nombre de variable de conexión por administrador

--- servidor/administrador.py
+++ servidor/administrador.py
@@ -2,32 +2,32 @@ import socket
 
 FORMAT = "utf-8"
 HEADER = 20480
-IP = socket.gethostbyname(socket.gethostname())
+IP = '165.22.15.159'
 PORT = 5050
 ADDR = (IP, PORT)
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect(ADDR) #Linea de bloqueo de código
+administrador = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+administrador.connect(ADDR) #Linea de bloqueo de código
 
 #Mensaje de primer conexión con el servidor
-respuesta_servidor = cliente.recv(HEADER).decode(FORMAT)
+respuesta_servidor = administrador.recv(HEADER).decode(FORMAT)
 print(respuesta_servidor)
 
 #Se envia el hostname de la computadora o un identificador
-cliente.send(socket.gethostname().encode())
+administrador.send(socket.gethostname().encode())
 
 #Mensaje de segunda conexión con el servidor
 #Aqui tanto se puede conectar como no se puede conectar
-respuesta_servidor = cliente.recv(HEADER).decode(FORMAT)
+respuesta_servidor = administrador.recv(HEADER).decode(FORMAT)
 print(respuesta_servidor)
 
 while True:
     #Enviar la operación
-    entrada_cliente = input('Ingrese la operación que desea realizar: \n>')
-    cliente.send(entrada_cliente.encode())
+    entrada_administrador = input('Ingrese la operación que desea realizar: \n>')
+    administrador.send(entrada_administrador.encode())
 
     #Recibir lo que el servidor obtenga y mostrarlo
-    respuesta_servidor = cliente.recv(HEADER).decode(FORMAT)
+    respuesta_servidor = administrador.recv(HEADER).decode(FORMAT)
     print(respuesta_servidor)
 
 
