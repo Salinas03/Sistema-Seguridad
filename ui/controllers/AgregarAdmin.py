ba72f634a6acbb73c5474817910588c8220728a0
DiegoRedesla <diegosalinasvazquez@gmail.com>
2023-05-30 11:37:06 -0600

    Funcionamiento de login
    Se agrego el funcionamiento de login, tambien la carpeta de modelos y se cambio user de la base de datos, por administradores

--- ui/controllers/AgregarAdmin.py
+++ ui/controllers/AgregarAdmin.py
@@ -1,6 +1,8 @@
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QWidget,QDialog,QLabel,QLineEdit,QMessageBox
 from views.AgregarAdmin import AgregarAdmin
 from PySide2.QtCore import Qt
+from db.administradores import registro_user
+
 
 class AgregarAdminWindow(AgregarAdmin, QWidget):
 
@@ -8,6 +10,27 @@ class AgregarAdminWindow(AgregarAdmin, QWidget):
         super().__init__(parent)
         self.setupUi(self)
         self.setWindowFlag(Qt.Window)
+        self.agregar_admin_btn.clicked.connect(self.agregar_admin)
+        self.cancelar_admin_btn.clicked.connect(self.cancelar_registro)
 
-    def mostrar():
-        pass
+
+    def agregar_admin(self):
+        nombre = self.nombre_admin_txt.text()
+        apellido = self.apellido_admin_txt.text()
+        telefono = self.telefono_admin_txt.text()
+        correo = self.correo_admin_txt.text()
+        password = self.password_admin_txt.text()
+        password2 = self.confirma_contrasenia_btn.text()
+
+        if nombre == '' or apellido == '' or telefono == '' or correo == '' or password == '' or password2 == '':
+            QMessageBox.warning(self, 'Error', 'Por favor de poner datos validos', QMessageBox.StandardButton.Close,QMessageBox.StandardButton.Close)
+        
+        elif password != password2:
+            QMessageBox.warning(self, 'Error', 'Las contraseñas no coinciden, favor de verificar',QMessageBox.StandardButton.Close,QMessageBox.StandardButton.Close)
+        
+        else:
+            self.registro_usuario = registro_user()
+            QMessageBox.warning(self, 'Registro', 'El registro se hizo con exito', QMessageBox.StandardButton.Close,QMessageBox.StandardButton.Close)
+
+    def cancelar_registro(self):
+        self.close()    
--- ui/controllers/Login.py
+++ ui/controllers/Login.py
@@ -1,7 +1,10 @@
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QWidget, QMessageBox
 from views.Login import Login
 from PySide2.QtCore import Qt
 from controllers.Principal import PrincipalWindow
+from db.connection import conexion
+from modelos.admin_model import Admin
+
 
 
 class LoginWindow(Login, QWidget):
@@ -9,16 +12,23 @@ class LoginWindow(Login, QWidget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.admin = Admin(conexion())
 
-        self.ingresar_btn.clicked.connect(self.abrir_principal_window)
+        #self.ingresar_btn.clicked.connect(self.abrir_principal_window)
+        self.x = self.ingresar_btn.clicked.connect(lambda:self.iniciar_sesion(self.correo_txt.text(),self.password_txt.text()))
 
+    def iniciar_sesion(self, correo, password):
+        if correo and password:
+            correo = self.admin.getAdmin(correo,password)
+            if correo:
+                self.abrir_principal_window()
+            else:
+                QMessageBox.warning(self, 'Error', 'Correo y/o contraseña invalidos', QMessageBox.StandardButton.Close,QMessageBox.StandardButton.Close)
+
+    
     def abrir_principal_window(self):
         self.close()
         window = PrincipalWindow(self)
         window.show()
         self.setWindowFlag(Qt.Window)
-
-
-    def iniciar_sesion(self):
-        pass
  
