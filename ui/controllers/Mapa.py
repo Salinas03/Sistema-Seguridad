e527d2d7ba44707f65a4cf63ead9d720c09d4dbc
DiegoRedesla <diegosalinasvazquez@gmail.com>
2023-07-06 13:38:52 -0600

    Funcionamiento de geolocalizacion, no exacta, pero funcional

--- ui/controllers/Mapa.py
+++ ui/controllers/Mapa.py
@@ -2,22 +2,24 @@ from PySide2.QtWidgets import *
 from PySide2 import QtWidgets, QtCore, QtWebEngineWidgets
 from views.Mapa import Mapa
 from PySide2.QtCore import *
-from utils.localizacion import Location
 import geocoder
 import folium
 
 class MapaWindow(Mapa, QWidget):
-    def __init__(self, parent = None):
+    def __init__(self, parent = None, ip_publica = None):
+        self.ip_publica = ip_publica
+        print(self.ip_publica)
         super().__init__(parent)
         self.setupUi(self)
         self.setWindowFlag(Qt.Window)
+        self.location()
 
         if self.location is not False:
             # Crear el widget QWebEngineView
             self.webview = QtWebEngineWidgets.QWebEngineView()
             
             # Configurar la URL inicial
-            self.webview.setUrl(QtCore.QUrl("http://127.0.0.1:5501/ui/html/mapa.html"))
+            self.webview.setUrl(QtCore.QUrl("http://127.0.0.1:5500/ui/html/mapa.html"))
             
             # Agregar el widget QWebEngineView a la ventana principal
             self.setCentralWidget(self.webview)
@@ -25,9 +27,8 @@ class MapaWindow(Mapa, QWidget):
             print("Error al obtener las coordenadas en el mapa")
 
 
-    def location():
-        ip = '177.249.10.8'
-        g = geocoder.ip(ip)
+    def location(self):
+        g = geocoder.ip(self.ip_publica)
 
         myAddress = g.latlng
         print(myAddress)
