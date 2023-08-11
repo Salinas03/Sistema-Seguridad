528d308920e5ccc0f087bcdc3c01fb55dd7f8794
DiegoRedesla <diegosalinasvazquez@gmail.com>
2023-08-11 13:44:50 -0600

    Cambios del servidor

--- ui/utils/manejar_consola.py
+++ ui/utils/manejar_consola.py
@@ -1,15 +1,16 @@
 import sys
 import socket
 import os
-sys.path.append(f'{os.getcwd()}/ui')
-from clases.administrador_ui import admin_socket_ui
+
+HEADER = 20480
+FORMAT = 'utf-8'
 
 #Socket para conectar con el proceso principal
 socket_subproceso = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_subproceso.connect(('127.0.0.1', 7777))
 
 #Recibimiento de prompt para imprimir en el input
-consola = socket_subproceso.recv(admin_socket_ui.HEADER).decode(admin_socket_ui.FORMAT)
+consola = socket_subproceso.recv(HEADER).decode(FORMAT)
 
 #Recibir input del administrador la primera vez que se abra la consola
 operacion = input(consola)
@@ -23,7 +24,7 @@ while True:
 
         if len(operacion) > 0:
             socket_subproceso.send(operacion.encode())
-            respuesta = socket_subproceso.recv(admin_socket_ui.HEADER).decode(admin_socket_ui.FORMAT)
+            respuesta = socket_subproceso.recv(HEADER).decode(FORMAT)
             print(respuesta, end='')
 
         #Recibir input del administrador de manera continua
